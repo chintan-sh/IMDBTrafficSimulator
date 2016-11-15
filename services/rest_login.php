6f01b0f5ad8747f28e614843d56ff526eb51457b
chintan <chintan.shah104@gmail.com>
2016-11-14 22:13:13 -0500

    Splitting login and signup services

---
+++ services/rest_login.php
@@ -0,0 +1,36 @@
+<?php
+include_once "/var/www/mdb/includes/common/constants.php";
+include_once $PHYSICAL_PATH . "includes/dao/userDAO.php";
+/**
+ * Created by PhpStorm.
+ * User: chintan
+ * Date: 11/10/16
+ * Time: 11:01 PM
+ */
+
+
+$mysqlObj = new UserDAO();
+$result = $mysqlObj->create_new_user("masoom", "masoom");
+if($result){
+    echo "User created successfully";
+    return;
+}
+
+echo "User creation failed";
+
+//$result = $mysqlObj->check_if_user_exists("chintan");
+//
+//if($result > 0){
+//    echo "Username found";
+//    $pass_result = $mysqlObj->check_if_password_is_valid("chintan", "chintan");
+//    echo "Did we find password? " . $pass_result;
+//}
+
+
+
+print_r($_POST);
+$name=$_POST["name"];
+$email=$_POST["email"];
+$username= $_POST["username"];
+$password= $_POST["password"];
+?>
