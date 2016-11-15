28b41d5862ee52e49140493f40fe51967bc5d984
SankalpJadon <sankalpjadon@gmail.com>
2016-11-15 11:17:26 -0500

    Tuesday morning updates

--- includes/dao/MovieDAO.php
+++ includes/dao/MovieDAO.php
@@ -7,26 +7,31 @@
  * Time: 11:14 PM
  */
 include_once '/var/www/html/mdb/includes/common/constants.php';
-include $Sankalp_Phy_Path.'/includes/db/mongo.php';
-include '/var/www/html/mdb/vendor/autoload.php';
+//include $Sankalp_Phy_Path.'/includes/db/mongo.php';
+//include '/var/www/html/mdb/vendor/autoload.php';
 
 class movieDAO
 {
     function insertMovie(){
-//      $dbObj= new Mongo1();
+        $conn = new MongoClient("mongodb://35.161.183.188:27017");
 
-        $mongoConnection = new MongoDB\Client("mongodb://admin:admin@35.161.183.188:27017");
-        $db = $mongoConnection->newdb;
+        $db = $conn->newdb;
 
-        $collection = $db->movies;
-//      echo ("collecction".$collection );
+        $username = "admin";
+        $password = "admin";
+
+        $db->authenticate($username, $password);
+        var_dump($db);
+        $collection= $db->movies;
+        var_dump($collection);
         $document = array(
             'title' => "Harry Potter",
             'Rating' => 100,
             'by' => "JK Rowling");
-        echo("here");
-        $collection->insertOne($document);
-        echo "Document inserted successfully";
+        echo("Inserting -> ");
+        var_dump($document);
+        $collection->insert($document, array("w" => 0));
+        var_dump($collection);
     }
 
 
@@ -35,14 +40,23 @@ class movieDAO
     }
 
     function getMovie(){
-        $dbObj= new Mongo();
-        $collection = $dbObj->movies;
-        echo "Movie collection selected";
+        $conn = new MongoClient("mongodb://35.161.183.188:27017");
 
+        $db = $conn->newdb;
+
+        $username = "admin";
+        $password = "admin";
+
+        $db->authenticate($username, $password);
+        var_dump($db);
+        $collection= $db->movies;
+        var_dump($collection);
         $cursor = $collection->find();
-        foreach ($cursor as $document) {
-            echo $document["year"] . "\n";
+        var_dump($cursor);
+        for ($i = 0; $i < 10; $i++) {
+            var_dump($cursor->getNext());
         }
+
     }
     
 }
--- includes/db/mongo.php
+++ includes/db/mongo.php
@@ -9,10 +9,9 @@
 include '/var/www/html/mdb/vendor/autoload.php';
 class Mongo1
 {
-    public function getMongoConnection(){
-        //$mongoConnection = new MongoDB\Driver\Manager("mongodb://35.161.183.188:27017");
-        $mongoConnection = new MongoDB\Client("mongodb://35.161.183.188:27017");
-        $db = $mongoConnection->selectDatabase('test');
-        return $db;
-    }
+//    public function getMongoConnection(){
+//        //$mongoConnection = new MongoDB\Driver\Manager("mongodb://35.161.183.188:27017");
+//        $mongoConnection = new MongoDB\Client("mongodb://admin:admin@35.161.183.188:27017"/newdb);
+//        return $mongoConnection;
+//    }
 }
